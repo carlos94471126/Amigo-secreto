c1f0c46ed99893595aa9c93bc9f73643e357edff
carlos94471126 <cdfb94471126@gmail.com.br>
2025-01-16 00:36:14 -0300

    função de adicionar nomes feito

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array para armazenar os amigos
+let amigos = [];
+let  i = 0;
+const Resultado = document.getElementById('resultado');
+const input = document.getElementById('amigo');
+
+function adicionarAmigo(){
+    i++
+    amigos[i] = input.value;
+    console.log(amigos[i]);
+    
+    if (input.value =="")
+    {
+        alert("Por favor digite um nome!");
+    }
+    input.value = '';
+    adicionarAmigo_front(amigos[i]);
+ 
+}
+function adicionarAmigo_front(nome)
+{
+    
+     const novoParagrafo = document.createElement('p');
+     novoParagrafo.textContent = nome;
+     const listaAmigos = document.getElementById('listaAmigos');
+     listaAmigos.appendChild(novoParagrafo);
+}
+
+
+
+
+function sortearAmigo()
+{
+    Resultado.innerHTML = "ganhamo";
+}
